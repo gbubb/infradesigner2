0a2f3822729ac823a6331c8a160fa6762edaf7ac
gbubb <gurbahn@gmail.com>
2025-04-14 14:33:53 +0200

    Cursor 2

--- src/components/design/QuantityDisplay.tsx
+++ src/components/design/QuantityDisplay.tsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import { CalculationBreakdown } from './CalculationBreakdown';
-import { Info, Calculator } from 'lucide-react';
+import { Info } from 'lucide-react';
 import { Badge } from '../ui/badge';
 import { Button } from '../ui/button';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 
 interface QuantityDisplayProps {
   roleId: string;
@@ -24,20 +22,7 @@ export const QuantityDisplay: React.FC<QuantityDisplayProps> = ({
         <Badge variant="outline" className="font-medium">
           {quantity}
         </Badge>
-        <CalculationBreakdown roleId={roleId} roleName={roleName}>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" size="icon" className="h-7 w-7 rounded-full hover:bg-blue-50 text-blue-500">
-                  <Calculator className="h-4 w-4" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="top">
-                <p>View calculation breakdown</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </CalculationBreakdown>
+        <CalculationBreakdown roleId={roleId} roleName={roleName} />
       </div>
     </div>
   );
